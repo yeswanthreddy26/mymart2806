521be095f42a35922c818a1d559db5e96b7756f9
yeswanthreddy26 <156288135+yeswanthreddy26@users.noreply.github.com>
2024-07-08 11:23:18 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,7 +2,7 @@
 
 cd /var/lib/jenkins/workspace/mymart
 
-#sudo cp -rf nginx.conf /etc/nginx/sites-available/
+sudo cp -rf nginx.conf /etc/nginx/sites-available/
 #sudo rm /etc/nginx/sites-enabled/nginx.conf
 #sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
 chmod 710 /var/lib/jenkins/workspace/mymart 
