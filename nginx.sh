4ba6ac8e87c56022c55928d2abe67cba665f0658
yeswanthreddy26 <156288135+yeswanthreddy26@users.noreply.github.com>
2024-07-08 11:07:43 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,12 +2,12 @@
 
 cd /var/lib/jenkins/workspace/mymart
 
-sudo cp -rf nginx.conf /etc/nginx/sites-available/
-sudo rm /etc/nginx/sites-enabled/nginx.conf
-sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
+#sudo cp -rf nginx.conf /etc/nginx/sites-available/
+#sudo rm /etc/nginx/sites-enabled/nginx.conf
+#sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
 chmod 710 /var/lib/jenkins/workspace/mymart 
 
-sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
+#sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
 
 sudo service nginx start
 
